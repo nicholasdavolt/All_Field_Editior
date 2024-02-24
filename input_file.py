bfc9d189cf2a4f43bb4cdb2d6f575efdf92cfc6e
nicholasdavolt <nicholasdavolt@gmail.com>
2024-02-24 11:24:16 -0600

    Completed file read

--- input_file.py
+++ input_file.py
@@ -1,2 +1,11 @@
-import os
+import csv
 
+
+def read_csv(file_path):
+    with open(file_path, newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=",", quotechar='"')
+        results = []
+        for row in csv_reader:
+            results.append(", ".join(row))
+        return results
+        
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from input_file import read_csv
+
 
 def main():
     file_path = input("Enter your path:")
@@ -7,9 +9,13 @@ def main():
     while not os.path.isfile(file_path):
         file_path = input("Filepath not valid, Re-Enter:")
     
-    print("File exists, beginning validation")
+    print("File exists, beginning import")
+
+    csv_contents = read_csv(file_path)
+
+    for row in csv_contents:
+        print(row)
     
-        
        
     
 
